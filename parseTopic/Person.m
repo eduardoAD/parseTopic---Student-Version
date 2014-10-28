a24add35270c6468b3d19cc3a8e5164f09906cba
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-28 11:26:23 -0600

    hackwich

---
+++ parseTopic/Person.m
@@ -0,0 +1,32 @@
+//
+//  Person.m
+//  parseTopicStudentVersion
+//
+//  Created by Eduardo Alvarado Díaz on 10/28/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+@dynamic age;
+@dynamic name;
+
++ (NSString *)parseClassName{
+    return @"Person";
+}
+
++ (void)load{
+    [self registerSubclass];
+}
+
+- (BOOL)isTeenager{
+    if (self.age.intValue >= 13 && self.age.intValue < 20) {
+        return YES;
+    }else{
+        return NO;
+    }
+}
+
+
+@end
